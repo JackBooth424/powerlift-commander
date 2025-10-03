4554759f4ee858be7471e12775a75a15f3d484c8
Jack Booth <jackbooth424@gmail.com>
2025-10-03 12:37:45 +0100

    ESP32 soldered and connected to MPU6050

--- sketch_aug15a.ino
+++ sketch_aug15a.ino
@@ -6,24 +6,29 @@ Adafruit_MPU6050 mpu;
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin(5, 4); // SDA = GPIO5, SCL = GPIO4
-  delay(1000);
 
-  Serial.println("Initializing MPU6050...");
+  // ESP32-C3: SDA = GPIO4, SCL = GPIO5
+  Wire.begin(4, 5); // Use Wire.setPins() if you use custom Wire instances
 
   if (!mpu.begin(0x68, &Wire)) {
-    Serial.println("Failed to find MPU6050 chip at 0x68");
-    while (1) { delay(10); }
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
   }
-  Serial.println("MPU6050 found!");
 
-  // Configure sensor ranges
+  Serial.println("MPU6050 Found!");
+
+  // Optional: configure accelerometer and gyroscope range
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  delay(100);
 }
 
 void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
+
 }
